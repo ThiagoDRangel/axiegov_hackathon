d2a8ce8d9ae0ada1b45eb81120a8aa1f3853c9a2
ThiagoDRangel <info.tec.campos@gmail.com>
2024-07-08 21:09:39 -0300

    fix: add background in Home page

---
+++ src/components/Tumb/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import background from '../../assets/images/moon.jpeg';
+import './styles.css';
+
+
+function Tumb () {
+    return (
+        <img src={background} alt="" />
+    );
+}
+
+export default Tumb;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,12 +3,14 @@ import Header from '../../components/Header';
 import './styles.css';
 import Footer from '../../components/Footer';
 import './styles.css';
+import Tumb from '../../components/Tumb';
 
 
 function Home() {
     return (
         <div className="container-home">
             <Header />
+            <Tumb />
             <main className="container-text">
                 <p>Welcome to the Home page</p>
                 <h1>hackathon</h1>
